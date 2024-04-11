16878f85ef888184c1940f2c9d851502d3878ed3
nikita <nicita.zinkevitch@yandex.ru>
2024-04-11 22:46:18 +0300

    Кодирование (экономия памяти) - исправлен баг

--- coder/encode.go
+++ coder/encode.go
@@ -1,6 +1,7 @@
 package coder
 
 import (
+	"fmt"
 	"slices"
 )
 
@@ -19,13 +20,14 @@ func Encode(seq []byte) ([]byte, int) {
 	slices.Sort(fcol)
 
 	var n int
+	var i int
 	var pos int
 	var groupCount int
 	var last = slen - 1
 	// сделать счетчик для одинаковых букв
 	// когда нашел одну букву, след такая же должна начинаться с той же позиции
-	for i := 0; i < slen; i++ {
-		if i == 0 || i == slen-1 || fcol[i] != fcol[i-1] {
+	for i = 0; i < slen; i++ {
+		if i != 0 && fcol[i] != fcol[i-1] {
 			slices.SortFunc(cycled, func(a, b []byte) int {
 				if a == nil && b != nil {
 					return 1
@@ -51,9 +53,13 @@ func Encode(seq []byte) ([]byte, int) {
 			groupCount = 0
 			clear(cycled)
 		}
+
 		for j := pos; j <= slen; j++ {
 			if j == slen {
 				j = 0
+			}
+			if fcol[i] == '_' {
+				fmt.Println()
 			}
 			if seq[j] == fcol[i] {
 				cycled[groupCount] = slices.Concat(seq[j:], seq[:j])
@@ -64,44 +70,27 @@ func Encode(seq []byte) ([]byte, int) {
 		}
 	}
 
-	/*// получение таблицы сдвигов
-	for i := 1; i < slen; i++ {
-		cycled[i] = make([]byte, slen)
-		j := i
-		m := 0
-		for m < slen {
-			if j == slen {
-				j = 0
-			}
-			cycled[i][m] = seq[j]
-			m++
-			j++
-		}
-	}
-
-	// лексикографическая сортировка
 	slices.SortFunc(cycled, func(a, b []byte) int {
-		for i := range a {
-			if a[i] > b[i] {
+		if a == nil && b != nil {
+			return 1
+		} else if b == nil && a != nil {
+			return -1
+		}
+		for k := range a {
+			if a[k] > b[k] {
 				return 1
-			} else if a[i] < b[i] {
+			} else if a[k] < b[k] {
 				return -1
 			}
 		}
 		return 0
 	})
-
-	// получить первый и последний столбец
-	// и найти номер исходной строки
-	last := slen - 1
-	var n int
-	for i := 0; i < slen; i++ {
-		lcol[i] = cycled[i][last]
-		if cycled[i][0] == seq[0] && cycled[i][last] == seq[last] {
-			if bytes.Compare(cycled[i], seq) == 0 {
-				n = i
-			}
+	for m := 0; m < groupCount; m++ {
+		if cycled[m] == nil {
+			break
 		}
-	}*/
+		lcol[i-groupCount+m] = cycled[m][last]
+	}
+
 	return lcol, n
 }
