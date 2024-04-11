c63a140beca35398f75503bf068d304fae40a0cc
nikita <nicita.zinkevitch@yandex.ru>
2024-04-11 22:08:37 +0300

    Кодирование (экономия памяти)

--- coder/encode.go
+++ coder/encode.go
@@ -1,7 +1,6 @@
 package coder
 
 import (
-	"bytes"
 	"slices"
 )
 
@@ -12,13 +11,60 @@ import (
 // но можно сравнивать не все сдвиги, а только те, что начинаются и заканчиваются на ту же букву
 func Encode(seq []byte) ([]byte, int) {
 	slen := len(seq)
-	var lcol []byte
-	var cycled [][]byte
+	var fcol, lcol []byte
+	var cycled = make([][]byte, slen)
 	lcol = make([]byte, slen)
-	cycled = make([][]byte, slen)
-	cycled[0] = seq
+	fcol = make([]byte, slen)
+	copy(fcol, seq)
+	slices.Sort(fcol)
 
-	// получение таблицы сдвигов
+	var n int
+	var pos int
+	var groupCount int
+	var last = slen - 1
+	// сделать счетчик для одинаковых букв
+	// когда нашел одну букву, след такая же должна начинаться с той же позиции
+	for i := 0; i < slen; i++ {
+		if i == 0 || i == slen-1 || fcol[i] != fcol[i-1] {
+			slices.SortFunc(cycled, func(a, b []byte) int {
+				if a == nil && b != nil {
+					return 1
+				} else if b == nil && a != nil {
+					return -1
+				}
+				for k := range a {
+					if a[k] > b[k] {
+						return 1
+					} else if a[k] < b[k] {
+						return -1
+					}
+				}
+				return 0
+			})
+			for m := 0; m < groupCount; m++ {
+				if cycled[m] == nil {
+					break
+				}
+				lcol[i-groupCount+m] = cycled[m][last]
+			}
+			pos = 0
+			groupCount = 0
+			clear(cycled)
+		}
+		for j := pos; j <= slen; j++ {
+			if j == slen {
+				j = 0
+			}
+			if seq[j] == fcol[i] {
+				cycled[groupCount] = slices.Concat(seq[j:], seq[:j])
+				groupCount++
+				pos = j + 1
+				break
+			}
+		}
+	}
+
+	/*// получение таблицы сдвигов
 	for i := 1; i < slen; i++ {
 		cycled[i] = make([]byte, slen)
 		j := i
@@ -56,7 +102,6 @@ func Encode(seq []byte) ([]byte, int) {
 				n = i
 			}
 		}
-	}
-
+	}*/
 	return lcol, n
 }
